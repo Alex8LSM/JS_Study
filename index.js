3608cb5f73ab5b4259489a7c34c7988badf1dee6
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-09-22 21:58:20 +0300

    Begin

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+// let a, b, c, d, e, f, g;
+// a = 10;
+// b = 5;
+// console.log("a=",a,"b=",b,"c=",c,"d=",d,"e=",e);
+// const getConsoleNumber = prompt("Введіть число");
+// console.log(getConsoleNumber);
+const text = "sjhfaksfbhkasfbaksfbkasfbasjf";
+
+console.log(text.slice(0, 5));

3d55790b23fee17437c95ff5007ba257c578e93f
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-10-04 05:44:03 +0300

    all

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-"use strict";
-// let a, b, c, d, e, f, g;
-// a = 10;
-// b = 5;
-// console.log("a=",a,"b=",b,"c=",c,"d=",d,"e=",e);
-// const getConsoleNumber = prompt("Введіть число");
-// console.log(getConsoleNumber);
-const text = "sjhfaksfbhkasfbaksfbkasfbasjf";
+function includes(array, value) {
+  // Change code below this line
+  for (let item of array) if (item === value) return true;
 
-console.log(text.slice(0, 5));
+  return false;
+  // Change code above this line
+}

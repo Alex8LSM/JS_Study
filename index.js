c864ac169a090ec73c4619ed958c7dbaad827464
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-10-12 20:29:42 +0300

    module 3 last task

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-function includes(array, value) {
+const atTheOldToad = {
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
   // Change code below this line
-  for (let item of array) if (item === value) return true;
 
-  return false;
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    for (const potion of this.potions) {
+      if (potion.name===newPotion.name) 
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+    }
+
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    const potionNames = [];
+    for (const potion of this.potions) 
+      potionNames.push(potion.name);
+
+    const potionIndex = potionNames.indexOf(potionName);
+
+    if (potionIndex === -1) {
+      return `Potion ${potionName} is not in inventory!`;
+    }
+
+    this.potions.splice(potionIndex, 1);
+  },
+  updatePotionName(oldName, newName) {
+    const potionNames = [];
+    for (const potion of this.potions)
+      potionNames.push(potion.name);
+
+    const potionIndex = potionNames.indexOf(oldName);
+
+    if (potionIndex === -1) {
+      return `Potion ${oldName} is not in inventory!`;
+    }
+
+    this.potions[potionIndex].name=newName;
+  },
   // Change code above this line
-}
+};
